b7e52dec6b93cfcf1f58050bfb2e5a52ecfd6817
Jarlok17 <abilik48@gmail.com>
2025-09-25 21:32:28 +0300

    feat: add PasswordManager, set application settings set icons for platforms

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,59 @@
+#include <QFile>
 #include <QGuiApplication>
+#include <QIcon>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 
 #include "clipboard/textclipboard.hpp"
 #include "dbmanager/dbmanager.hpp"
+#include "passwordmanager/passwordmanager.hpp"
 #include "usermanager/usermanager.hpp"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    app.setApplicationName("PM");
+    app.setApplicationVersion("1.0.0");
+    app.setOrganizationName("Zero");
+
+#ifdef Q_OS_WIN
+    QString iconPath = ":/icons/app/icon.ico";
+#else
+    QString iconPath = ":/icons/app/icon.png";
+#endif
+
+    qDebug() << "Loading icon from:" << iconPath;
+    if (QFile::exists(iconPath)) {
+        qDebug() << "Icon file exists";
+        app.setWindowIcon(QIcon(iconPath));
+    } else {
+        qDebug() << "Icon file does NOT exist";
+        // Спробуйте альтернативний шлях
+        iconPath = "qrc:/icons/app/icon.png";
+        qDebug() << "Trying alternative path:" << iconPath;
+        app.setWindowIcon(QIcon(iconPath));
+    }
+
     QQmlApplicationEngine engine;
 
     qmlRegisterType<TextClipboard>("PM", 1, 0, "TextClipboard");
     qmlRegisterType<DBManager>("PM", 1, 0, "DBManager");
     qmlRegisterType<UserManager>("PM", 1, 0, "UserManager");
+    qmlRegisterType<PasswordManager>("PM", 1, 0, "PasswordManager");
 
     // Create managers
     DBManager dbManager;
     UserManager userManager(&dbManager);
+    PasswordManager passwordManager(&dbManager);
 
     // Add them to context for global access
     engine.rootContext()->setContextProperty("dbManager", &dbManager);
     engine.rootContext()->setContextProperty("userManager", &userManager);
+    engine.rootContext()->setContextProperty("passwordManager", &passwordManager);
+
+    // Connect user manager to password manager
+    QObject::connect(&userManager, &UserManager::currentUserIdChanged, &passwordManager, &PasswordManager::setCurrentUserId);
 
     const QUrl url(QStringLiteral("qrc:/Main.qml"));
     QObject::connect(
